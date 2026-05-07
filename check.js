4bb415480cb941b746a51ae2ebe011cead2df366
Christian Fehmer <cfe@sexy-developer.com>
2026-05-07 09:26:16 +0200

    fix

--- check.js
+++ check.js
@@ -1,10 +1,24 @@
-const fs = require("node:fs");
+import * as fs from "node:fs";
+import * as ghCore from "@actions/core";
+
 const ENV_SUMMARY = process.env.GITHUB_STEP_SUMMARY;
 const lines = [];
 
 function printSummary() {
   if (ENV_SUMMARY !== null) {
-    fs.writeFileSync(ENV_SUMMARY, lines.join("\n"));
+    ghCore.summary
+      .addHeading("Test Results")
+      .addTable([
+        [
+          { data: "", header: true },
+          { data: "File", header: true },
+          { data: "Result", header: true },
+        ],
+        ["Pass ✅", "", ""],
+        ["Fail ❌", "click15/1.wav", "additional file"],
+        ["Fail ❌", "click15/2.wav", "missing file"],
+      ])
+      .write();
   }
 }
 
